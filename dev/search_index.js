ad7f93aeb13d815b6b0c5782808e998bf5ec9a25
Documenter.jl <documenter@juliadocs.github.io>
2022-09-03 20:23:06 +0000

    build based on 26fe7fd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RDataGet","category":"page"},{"location":"#RDataGet","page":"Home","title":"RDataGet","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RDataGet.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RDataGet]","category":"page"}]
+}
